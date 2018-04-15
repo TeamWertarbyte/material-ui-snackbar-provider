91e42183988b5c191750784caac13649c8087f7e
Samir HABIB ZAHMANI <unforgiven-wanda@hotmail.fr>
2018-04-15 15:58:56 +0100

    changed material-ui-icons with @material-ui/icons

--- src/SnackbarProvider.js
+++ src/SnackbarProvider.js
@@ -1,18 +1,18 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import Snackbar from 'material-ui/Snackbar'
-import Button from 'material-ui/Button'
-import IconButton from 'material-ui/IconButton'
-import CloseIcon from 'material-ui-icons/Close'
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import Snackbar from "material-ui/Snackbar";
+import Button from "material-ui/Button";
+import IconButton from "material-ui/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 export default class SnackbarProvider extends PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       message: null,
       open: false,
       showCloseButton: false
-    }
+    };
   }
 
   getChildContext() {
@@ -20,7 +20,7 @@ export default class SnackbarProvider extends PureComponent {
       snackbar: {
         showMessage: this.showMessage
       }
-    }
+    };
   }
 
   /**
@@ -31,59 +31,67 @@ export default class SnackbarProvider extends PureComponent {
    * @public
    */
   showMessage = (message, action, handleAction, showCloseButton) => {
-    this.setState({ open: true, message, action, handleAction, showCloseButton })
-  }
+    this.setState({
+      open: true,
+      message,
+      action,
+      handleAction,
+      showCloseButton
+    });
+  };
 
   handleActionClick = () => {
-    this.handleClose()
-    this.state.handleAction()
-  }
+    this.handleClose();
+    this.state.handleAction();
+  };
 
   handleClose = () => {
-    this.setState({ open: false, handleAction: null })
-  }
+    this.setState({ open: false, handleAction: null });
+  };
 
   render() {
-    const {
-      action,
-      message,
-      open,
-      showCloseButton
-    } = this.state
+    const { action, message, open, showCloseButton } = this.state;
 
-    const {
-      children,
-      snackbarProps = {},
-      style = {}
-    } = this.props
+    const { children, snackbarProps = {}, style = {} } = this.props;
 
     let actions = [];
     if (action != null)
-      actions.push(<Button key="button" color='secondary' size='small' onClick={this.handleActionClick}> {action}</Button>)
+      actions.push(
+        <Button
+          key="button"
+          color="secondary"
+          size="small"
+          onClick={this.handleActionClick}
+        >
+          {" "}
+          {action}
+        </Button>
+      );
     if (showCloseButton)
-      actions.push(<IconButton
-        style={{ width: '32px', height: '32px' }}
-        key="close"
-        aria-label="Close"
-        color="inherit"
-        onClick={this.handleClose}
-      >
-        <CloseIcon />
-      </IconButton>)
-
+      actions.push(
+        <IconButton
+          style={{ width: "32px", height: "32px" }}
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          onClick={this.handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      );
 
     return (
-      <div style={{ width: 'inherit', height: 'inherit', ...style }}>
+      <div style={{ width: "inherit", height: "inherit", ...style }}>
         {children}
         <Snackbar
           {...snackbarProps}
           open={open}
-          message={message || ''}
+          message={message || ""}
           action={actions}
           onClose={this.handleClose}
         />
       </div>
-    )
+    );
   }
 }
 
@@ -91,7 +99,7 @@ SnackbarProvider.childContextTypes = {
   snackbar: PropTypes.shape({
     showMessage: PropTypes.func
   })
-}
+};
 
 SnackbarProvider.propTypes = {
   /**
@@ -106,4 +114,4 @@ SnackbarProvider.propTypes = {
    * Override the inline-styles of the root element.
    */
   style: PropTypes.object
-}
+};
